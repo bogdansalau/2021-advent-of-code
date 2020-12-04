7d08c01cd51661ef463b18c003826eb0b35c4287
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2020-12-04 18:37:45 +0200

    add day 3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 import day2.countValidPasswords
+import day3.countTrees
 import io.readFileDirectlyAsText
+import io.readStringArray
 
 fun main() {
-    val inputStr = readFileDirectlyAsText("day2\\input.txt")
-    println(countValidPasswords(inputStr))
+    val inputStr = readStringArray("day3\\input.txt")
+    println(countTrees(1, 2, inputStr))
+
+    // 63, 181, 55, 67, 30
 }
--- src/main/kotlin/io/FileReader.kt
+++ src/main/kotlin/io/FileReader.kt
@@ -4,12 +4,14 @@ import java.io.File
 
 fun readArray(file: String): List<Int> {
     return readFileDirectlyAsText(file)
+        .trim()
         .split("\n")
         .map { try { it.toInt() } catch (e: NumberFormatException) { Integer.MAX_VALUE } }
 }
 
 fun readStringArray(file: String): List<String> {
     return readFileDirectlyAsText(file)
+        .trim()
         .split("\n")
 }
 
