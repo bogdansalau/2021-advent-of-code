9d8462d16ec9d5e9c6f0a432dd4490b6b029171b
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2020-12-14 17:03:30 +0200

    add day 14

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,15 @@
 import day12.RainRisk
 import day13.ShuttleSearch
+import day14.DockingData
 import io.readStringArray
+import util.pow
 import util.println
 
 fun main() {
-    val inputStr = readStringArray("day12\\input.txt")
-    val day13 = RainRisk(inputStr)
-    day13.solvePart2().println()
+    val inputStr = readStringArray("day14\\input.txt")
+    val day14 = DockingData(inputStr)
+    day14.solvePart2().println()
 
+
+//    println(2L.pow(2))
 }
--- src/main/kotlin/util/Extensions.kt
+++ src/main/kotlin/util/Extensions.kt
@@ -11,3 +11,13 @@ fun Long.println() {
 fun String.println() {
     println(this)
 }
+
+fun Long.pow(power: Long): Long {
+    if (power == 0L) return 1
+    if (power == 1L) return this
+    var acc = 1L
+    for(i in 0 until power) {
+        acc *= this
+    }
+    return acc
+}
