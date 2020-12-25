0c4fb88a9753ca665af73d766cd9a45e479423ac
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2020-12-25 16:53:17 +0200

    add day 23

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,13 @@
 import day21.AllergenAssessment
 import day22.CrabCombat
 import day22.Day22
+import day23.Day23
+import io.readFileAsString
 import io.readFileAsStringArray
 import util.println
 
 fun main() {
-    val inputStr = readFileAsStringArray("day22\\input.txt")
-    val day = Day22(inputStr)
-    day.solvePart2().println()
+    val inputStr = readFileAsString("day23\\input.txt")
+    val day = Day23(inputStr.trim())
+    day.solvePart2(10_000_000).println()
 }
