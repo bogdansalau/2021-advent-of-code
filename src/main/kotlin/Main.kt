0e9ddf5f875ea6d0ac5d0ed45a39ac0eade62a0b
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2020-12-26 13:59:55 +0200

    add day 25

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,15 @@ import day22.CrabCombat
 import day22.Day22
 import day23.Day23
 import day24.Day24
+import day25.ComboBreaker
 import io.readFileAsString
 import io.readFileAsStringArray
 import util.println
 
 fun main() {
-//    val inputStr = readFileAsStringArray("day24\\input.txt")
-//    val day = Day24(inputStr)
-//    day.solvePart2().println()
+    val inputStr = readFileAsStringArray("day25\\input.txt")
+    val day = ComboBreaker(inputStr)
+    day.solvePart1().println()
 
 
-    val splitPattern = "([ns]?[ew])".toRegex()
-    val seq = splitPattern.findAll("eenweesewseweene").map { it.value }
-    println()
 }
