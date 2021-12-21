05bdc9b2584c84c6e78c9e46ee1b30749bec201a
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2021-12-21 21:41:15 +0200

    Solve day 3 part 1

--- src/main/kotlin/day3/BinaryDiagnostic.kt
+++ src/main/kotlin/day3/BinaryDiagnostic.kt
@@ -1,4 +1,47 @@
 package day3
 
-class BinaryDiagnostic {
+class BinaryDiagnostic(input: List<String>) {
+
+    private val commands = input
+
+    fun solvePart1(): Int {
+        var zeros = List(commands[0].length) { 0 }
+        var ones = List(commands[0].length) { 0 }
+
+        commands.forEach {
+            zeros = countChar(zeros, it, '0')
+            ones = countChar(ones, it, '1')
+        }
+
+        val gamma = getGamma(zeros, ones)
+
+        val epsilon = getEpsilon(zeros, ones)
+
+        return gamma * epsilon
+    }
+
+    fun getGamma(
+        zeros: List<Int>,
+        ones: List<Int>
+    ) = zeros.zip(ones) { zero, one -> if (zero > one) "0" else "1" }
+        .reduce { acc, c -> acc + c }
+        .toInt(2)
+
+    fun getEpsilon(
+        zeros: List<Int>,
+        ones: List<Int>
+    ) = zeros.zip(ones) { zero, one -> if (zero <= one) "0" else "1" }
+        .reduce { acc, c -> acc + c }
+        .toInt(2)
+
+    fun countChar(zeros: List<Int>, s: String, toCount: Char): List<Int> {
+        return zeros.zip(s.toCharArray().asIterable()) { freq, char ->
+            freq + if (char == toCount) 1 else 0
+        }
+    }
+
+//    fun solvePart2(): Int {
+//
+//    }
+
 }
--- src/test/kotlin/day3/BinaryDiagnosticTest.kt
+++ src/test/kotlin/day3/BinaryDiagnosticTest.kt
@@ -1,21 +1,70 @@
 package day3
 
-import day2.Dive
 import io.readFileAsStringArray
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
-import util.println
+import java.io.File
 
-internal class BinaryDiagnosticTest{
+internal class BinaryDiagnosticTest {
+    private val input = listOf("000111")
+
+    private val binaryDiagnostic = BinaryDiagnostic(input)
+
     @Test
-    fun `first test`() {
-        assertEquals(1, 1)
+    fun `countChars increments zeros number in frequency vector`() {
+        val zeros = List(6) { 0 }
+
+        val result = binaryDiagnostic.countChar(zeros, "000111", '0')
+
+        assertEquals(listOf(1, 1, 1, 0, 0, 0), result)
     }
 
     @Test
-    fun `file text`() {
-        val inputStr = readFileAsStringArray("day2\\input.txt")
-        val day = Dive(inputStr)
-        day.solvePart2().println()
+    fun `countChars increments ones number in frequency vector`() {
+        val zeros = List(6) { 0 }
+
+        val result = binaryDiagnostic.countChar(zeros, "000111", '1')
+
+        assertEquals(listOf(0, 0, 0, 1, 1, 1), result)
+    }
+
+    @Test
+    fun `getGamma returns decimal formed with most common bit in input`() {
+        val zeros = listOf(1, 0, 0)
+        val ones = listOf(0, 1, 1)
+
+        val result = binaryDiagnostic.getGamma(zeros, ones)
+
+        assertEquals(3, result)
+    }
+
+    @Test
+    fun `getEpsilon returns decimal formed with least common bit in input`() {
+        val zeros = listOf(1, 0, 0)
+        val ones = listOf(0, 1, 1)
+
+        val result = binaryDiagnostic.getEpsilon(zeros, ones)
+
+        assertEquals(4, result)
+    }
+
+    @Test
+    fun `example input`() {
+        val exampleInput = readFileAsStringArray("src\\test\\resources\\day3\\example.txt")
+        val binaryDiagnostic = BinaryDiagnostic(exampleInput)
+
+        val result = binaryDiagnostic.solvePart1()
+
+        assertEquals(198, result)
+    }
+
+    @Test
+    fun `official input`() {
+        val exampleInput = readFileAsStringArray("src\\test\\resources\\day3\\input.txt")
+        val binaryDiagnostic = BinaryDiagnostic(exampleInput)
+
+        val result = binaryDiagnostic.solvePart1()
+
+        assertEquals(2003336, result)
     }
 }
