06d280cc8c3a8f7dba20f553acdaf07620817135
Bogdan Salau <Bogdan.Salau@cloudflight.io>
2021-12-28 01:21:59 +0200

    Solve day 6 part 1 and 2

--- src/main/kotlin/day6/Lanternfish.kt
+++ src/main/kotlin/day6/Lanternfish.kt
@@ -1,15 +1,57 @@
 package day6
 
-class Lanternfish(input: List<String>) {
-    private val linesAsPointPair = input[0].split(",").map { it.toInt() }
+import java.math.BigInteger
 
 
-    fun solvePart1(): Int {
-        return 0
+class Lanternfish(input: List<String>, private val daysToSimulate: Int) {
+    private val fish = input[0].split(",").map { it.toInt() }
+    private val fishDueDates = ArrayDeque<BigInteger>(9)
+        .apply {
+            repeat(9) {
+                this.add(BigInteger.ZERO)
+            }
+        }
+        .apply {
+            fish.forEach {
+                this[it] = this[it].inc()
+            }
+        }
+
+    fun solvePart1(ticks: Int): Int {
+        var state = fish
+
+        (0 until ticks).forEach { _ ->
+            state = tick(state)
+        }
+
+        return state.size
     }
 
-    fun solvePart2(): Int {
-        return 0
+    private fun tick(currentState: List<Int>): List<Int> {
+        val nextState = mutableListOf<Int>()
+        currentState.forEach {
+            when (it) {
+                in 1..8 -> nextState.add(it - 1)
+                0 -> {
+                    nextState.add(8)
+                    nextState.add(6)
+                }
+            }
+        }
+        return nextState.toList()
+    }
+
+    fun solvePart2(): BigInteger {
+        return simulateDays(fishDueDates, daysToSimulate)
+    }
+
+    private fun simulateDays(fishDueDates: ArrayDeque<BigInteger>, daysToSimulate: Int): BigInteger {
+        repeat(daysToSimulate) {
+            val dueNow = fishDueDates.removeFirst()
+            fishDueDates[6] = fishDueDates[6].add(dueNow)
+            fishDueDates.addLast(dueNow)
+        }
+        return fishDueDates.sumOf { it }
     }
 
 }
--- src/test/kotlin/day6/LanternfishTest.kt
+++ src/test/kotlin/day6/LanternfishTest.kt
@@ -3,26 +3,37 @@ package day6
 import io.readFileAsStringArray
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 internal class LanternfishTest {
     @Test
-    fun `example input part1`() {
+    fun `example1 input part1`() {
         val exampleInput = readFileAsStringArray("src\\test\\resources\\day6\\example.txt")
-        val lanternfish = Lanternfish(exampleInput)
+        val lanternfish = Lanternfish(exampleInput, 80)
 
-        val result = lanternfish.solvePart1()
+        val result = lanternfish.solvePart1(18)
 
-        assertEquals(5, result)
+        assertEquals(26, result)
+    }
+
+    @Test
+    fun `example2 input part1`() {
+        val exampleInput = readFileAsStringArray("src\\test\\resources\\day6\\example.txt")
+        val lanternfish = Lanternfish(exampleInput, 80)
+
+        val result = lanternfish.solvePart1(80)
+
+        assertEquals(5934, result)
     }
 
     @Test
     fun `official input part1`() {
         val officialInput = readFileAsStringArray("src\\test\\resources\\day6\\input.txt")
-        val lanternfish = Lanternfish(officialInput)
+        val lanternfish = Lanternfish(officialInput, 80)
 
-        val result = lanternfish.solvePart1()
+        val result = lanternfish.solvePart1(80)
 
-        assertEquals(6225, result)
+        assertEquals(380243, result)
     }
 
     //    ----- Part 2
@@ -30,20 +41,20 @@ internal class LanternfishTest {
     @Test
     fun `example input part2`() {
         val exampleInput = readFileAsStringArray("src\\test\\resources\\day6\\example.txt")
-        val lanternfish = Lanternfish(exampleInput)
+        val lanternfish = Lanternfish(exampleInput, 256)
 
         val result = lanternfish.solvePart2()
 
-        assertEquals(12, result)
+        assertEquals(BigInteger("26984457539"), result)
     }
 
     @Test
     fun `official input part2`() {
         val officialInput = readFileAsStringArray("src\\test\\resources\\day6\\input.txt")
-        val lanternfish = Lanternfish(officialInput)
+        val lanternfish = Lanternfish(officialInput, 256)
 
         val result = lanternfish.solvePart2()
 
-        assertEquals(22116, result)
+        assertEquals(BigInteger("1708791884591"), result)
     }
 }
